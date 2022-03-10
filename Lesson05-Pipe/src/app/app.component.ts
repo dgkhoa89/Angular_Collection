419398ec9fcd1029569f7721347dc31e89a0cdcd
dgkhoa89_local <dgkhoa89@gmail.com>
2022-03-11 00:03:28 +0700

    create lesson 5 with pipe upper, lower, title case

---
+++ Lesson05-Pipe/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+public content : string = `Dublin, capital of the Republic of Ireland, is on Ireland’s east coast at the mouth of the River Liffey. Its historic buildings include Dublin Castle, dating to the 13th century, and imposing St Patrick’s Cathedral, founded in 1191. City parks include landscaped St Stephen’s Green and huge Phoenix Park, containing Dublin Zoo. The National Museum of Ireland explores Irish heritage and culture.`;
+  title = 'Lesson05-Pipe';
+}
