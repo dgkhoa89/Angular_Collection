77d24ca45244a6819f6b098ae4a4be16f23540be
dgkhoa89_local <dgkhoa89@gmail.com>
2022-03-11 11:28:27 +0700

    pipe currency and date

--- Lesson05-Pipe/src/app/app.component.ts
+++ Lesson05-Pipe/src/app/app.component.ts
@@ -10,4 +10,6 @@ export class AppComponent {
   public content: string = `Dublin, capital of the Republic of Ireland, is on Ireland’s east coast at the mouth of the River Liffey. Its historic buildings include Dublin Castle, dating to the 13th century, and imposing St Patrick’s Cathedral, founded in 1191. City parks include landscaped St Stephen’s Green and huge Phoenix Park, containing Dublin Zoo. The National Museum of Ireland explores Irish heritage and culture.`;
   public amount:number = 35.145664;
   public names :string[]=['aaa','bbb','ccc'];
+  public amountMoney:number = 75.8;
+  public today: Date = new Date();
 }
