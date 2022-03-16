61bf5cac8466e483293271f03082a8444cf57660
leda <dat.le@iconplc.com>
2022-03-17 00:53:34 +0700

    lifeCycle hook - onchanges - input out put between parent and child

--- Lesson09-LifeCycleHook/src/app/app.component.ts
+++ Lesson09-LifeCycleHook/src/app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lesson09-LifeCycleHook';
+
   public isShowing: boolean = true;
-  onToggle(){
-    this.isShowing = !this.isShowing; 
+
+  onToggle() {
+    this.isShowing = !this.isShowing;
   }
+
+  public sumValueParent: number = 0;
+
+  onSummary(a: any, b: any) {
+    this.sumValueParent = (+a + +b);
+  }
+
+  updateCount(value: Event){
+      console.log('parent - value received from child is : ' + value);      
+  }
+
 }
--- Lesson09-LifeCycleHook/src/app/components/life-cycle-hook/life-cycle-hook.component.ts
+++ Lesson09-LifeCycleHook/src/app/components/life-cycle-hook/life-cycle-hook.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, OnChanges,Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-life-cycle-hook',
@@ -7,18 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LifeCycleHookComponent implements OnInit {
 
+  title:string='Child'
+
+  @Input() sumValue:number = 0;
+
+  @Output() newItemEvent  = new EventEmitter<any>();
+  addNewItem(value:any) {
+    this.newItemEvent.emit(value);
+  }
+
+  countValue = 0;
+
   constructor() { 
     console.log('constructor ....');
-    
-
   }
 
   ngOnInit(): void {
     console.log('ng Oninit - .....');
     
   }
-  ngOnChange(): void {
-    console.log('ng OnChange - .....');
+  ngOnChanges(): void {
+    this.countValue++;
+    console.log('child - on change- count is: ' + this.countValue);
+    this.addNewItem(this.countValue);
   }
   ngOnDestroy(): void {
     console.log('ng OnDestroy - .....');
